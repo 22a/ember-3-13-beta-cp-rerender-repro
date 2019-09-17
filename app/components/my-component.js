9cae3a4beb94d95c0763b13b25c5227155061ce7
Peter Meehan <peter.meehan@intercom.io>
2019-09-17 10:30:52 +0100

    attempt raw ember repro

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
+import { and } from '@ember/object/computed';
 
 export default Component.extend({
+  x: {
+    a: {
+      b: {
+        c: true
+      }
+    }
+  },
+  y: {
+    a: {
+      b: {
+        c: true
+      }
+    }
+  },
+
+  myProp: and('x.a.b.c', 'y.a.b.c'),
 });
