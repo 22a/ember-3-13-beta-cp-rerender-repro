737b98e230a87af81e5c3bcdc025c1b92e525de4
Pat O'Callaghan <pat@intercom.io>
2019-09-17 11:46:56 +0100

    Refactor test and code

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -1,6 +1,15 @@
 import Component from '@ember/component';
-import { and } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  myProp: and('cheese', 'model.validations.isValid'),
+  // myProp: and('cheese', 'model.validations.isValid'),
+  init() {
+    this._super(...arguments);
+    this.myProp;
+    this.model.validations.isValid;
+  },
+  cheese: true,
+  myProp: computed('cheese', 'model.validations.isValid', function() {
+    return this.cheese;
+  }),
 });
--- tests/integration/components/my-component-test.js
+++ tests/integration/components/my-component-test.js
@@ -1,16 +1,13 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { setupTest } from 'ember-qunit';
 
-module('Integration | Component | my-component', function(hooks) {
-  setupRenderingTest(hooks);
+module('Unit | Component | my-component', function(hooks) {
+  setupTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('model', this.owner.lookup('service:store').createRecord('aaa'));
+    this.set('model', this.owner.lookup('service:store').createRecord('user'));
 
-    await render(hbs`<MyComponent @model={{this.model}}/>`);
-
+    this.owner.factoryFor('component:my-component').create({ model: this.model });
     assert.ok(true);
   });
 });
